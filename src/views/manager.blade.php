b6658ca98f4fcbcfa7f3483415888335b47b0b61
kouceyla <hadjikouceyla@gmail.com>
2019-12-01 12:01:32 +0100

    Add title in SEO fix #3

--- src/views/manager.blade.php
+++ src/views/manager.blade.php
@@ -1,4 +1,7 @@
 	<!-- Standard SEO -->
+	@isset($title)
+		<title>{{ $title }}</title>
+	@endisset
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="referrer" content="{{ !empty($referrer)? $referrer : config('meta.referrer') }}">
 	<meta name="robots" content="{{ !empty($robots)? $robots : config('meta.robots') }}">
